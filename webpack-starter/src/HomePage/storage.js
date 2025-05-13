79a92312d9880cdeaa5237661a55accc25da6ac0
DC75166 <daipayanchakraborty75@gmail.com>
2025-05-13 08:15:06 +0530

    Storage working logic implemented

--- webpack-starter/src/HomePage/storage.js
+++ webpack-starter/src/HomePage/storage.js
@@ -7,15 +7,34 @@ export function saveToStorage(projectList){
     localStorage.setItem(STORAGE_KEY,JSON.stringify(projectList));
 }
 
-export function getFromStorage(manager){
-    const savedProject = JSON.parse(localStorage.getItem(STORAGE_KEY))||[];
-    savedProject.forEach(projectData=>{
-        const project = new Project(projectData.title,projectData.description,projectData.id);
-        project.todos = (projectData.todos || []).map(todoData => {
-            const todo = new Todo(todoData.title, todoData.description, todoData.priority, todoData.dueDate);
-            todo.completed = todoData.completed;
-            return todo;
+export function getFromStorage(manager) {
+    const savedProject = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    // Handle if it's an object instead of an array
+    if (savedProject && Array.isArray(savedProject)) {
+        savedProject.forEach(projectData => {
+            const project = new Project(projectData.title, projectData.description, projectData.id);
+            project.todos = (projectData.todos || []).map(todoData => {
+                const todo = new Todo(todoData.title, todoData.description, todoData.priority, todoData.dueDate);
+                todo.completed = todoData.completed;
+                return todo;
+            });
+            manager.add(project);
         });
-        manager.add(project);
-    })
+    } else if (savedProject && typeof savedProject === 'object') {
+        // If it's an object, wrap it in an array
+        const projectArray = Object.values(savedProject);
+        projectArray.forEach(projectData => {
+            const project = new Project(projectData.title, projectData.description, projectData.id);
+            project.todos = (projectData.todos || []).map(todoData => {
+                const todo = new Todo(todoData.title, todoData.description, todoData.priority, todoData.dueDate);
+                todo.completed = todoData.completed;
+                return todo;
+            });
+            manager.add(project);
+        });
+    } else {
+        console.log("No valid projects found in storage.");
+    }
 }
+
